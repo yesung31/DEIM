e66da39bd88e9ad099e7e10dc4c393b3573529a4
Yesung Hwang <yesung.hw@gmail.com>
2025-03-19 10:52:07 +0100

    Add DriveU to COCO converter

---
+++ driveu_annotations.py
@@ -0,0 +1,93 @@
+import json
+import os
+from copy import deepcopy
+from pathlib import Path
+
+
+def convert_to_coco(input_file, output_dir):
+    with open(input_file, "r") as f:
+        data = json.load(f)
+
+    coco_format_template = {
+        "images": [],
+        "annotations": [],
+        "categories": [
+            {"id": 0, "name": "off-relevant", "supercategory": "traffic_light"},
+            {"id": 1, "name": "red-relevant", "supercategory": "traffic_light"},
+            {"id": 2, "name": "yellow-relevant", "supercategory": "traffic_light"},
+            {"id": 3, "name": "redyellow-relevant", "supercategory": "traffic_light"},
+            {"id": 4, "name": "green-relevant", "supercategory": "traffic_light"},
+            {"id": 5, "name": "off-irrelevant", "supercategory": "traffic_light"},
+            {"id": 6, "name": "red-irrelevant", "supercategory": "traffic_light"},
+            {"id": 7, "name": "yellow-irrelevant", "supercategory": "traffic_light"},
+            {"id": 8, "name": "redyellow-irrelevant", "supercategory": "traffic_light"},
+            {"id": 9, "name": "green-irrelevant", "supercategory": "traffic_light"},
+        ],
+    }
+
+    sets = ["train", "val", "test"]
+    coco_formats = {s: deepcopy(coco_format_template) for s in sets}
+    annotation_id = {s: 1 for s in sets}
+
+    state = {"off": 0, "red": 1, "yellow": 2, "red_yellow": 3, "green": 4, "unknown": 0}
+    relevance = {"relevant": 0, "not_relevant": 5}
+
+    for image_id, image_data in enumerate(data["images"]):
+        image_path = image_data["image_path"]
+        image_filename = os.path.basename(image_path)
+        set_name = None
+
+        for s in sets:
+            file_path = Path(f"./dataset/images/{s}/{image_filename}")
+            if file_path.exists() or file_path.is_symlink():
+                set_name = s
+                break
+
+        if set_name is None:
+            continue
+
+        coco_formats[set_name]["images"].append(
+            {
+                "id": image_id,
+                "file_name": image_filename,
+                "width": 2048,
+                "height": 1024,
+            }
+        )
+
+        for label in image_data["labels"]:
+            if (
+                label["attributes"]["direction"] != "front"
+                or label["attributes"]["reflection"] == "reflected"
+            ):
+                continue
+
+            category_id = (
+                state[label["attributes"]["state"]]
+                + relevance[label["attributes"]["relevance"]]
+            )
+
+            bbox = [label["x"], label["y"], label["w"], label["h"]]
+            coco_formats[set_name]["annotations"].append(
+                {
+                    "id": annotation_id[set_name],
+                    "image_id": image_id,
+                    "category_id": category_id,
+                    "bbox": bbox,
+                    "area": label["w"] * label["h"],
+                    "iscrowd": 0,
+                }
+            )
+            annotation_id[set_name] += 1
+
+    for s in sets:
+        output_file = os.path.join(output_dir, f"instances_{s}.json")
+        with open(output_file, "w") as f:
+            json.dump(coco_formats[s], f, indent=4)
+
+
+if __name__ == "__main__":
+    # Usage example
+    convert_to_coco(
+        "/home/mne/Datasets/DriveU/v2.0/DTLD_all.json", "./dataset/annotations"
+    )
---
+++ driveu_images.py
@@ -0,0 +1,55 @@
+import json
+import os
+import random
+import shutil
+
+
+def make_simlinks(file_path, split):
+    with open(file_path, "r") as file:
+        data = json.load(file)
+        if "images" in data and isinstance(data["images"], list):
+            for image in data["images"]:
+                if "image_path" in image:
+                    old_path = image["image_path"]
+                    new_path = os.path.join("/home/mne/Datasets/DriveU", old_path)
+                    new_path = os.path.normpath(new_path)
+
+                    # Create symlink
+                    symlink_path = os.path.join(
+                        f"./dataset/images/{split}", os.path.basename(old_path)
+                    )
+                    symlink_path = os.path.normpath(symlink_path)
+                    if not os.path.exists(symlink_path):
+                        os.symlink(new_path, symlink_path)
+
+            # Save the modified JSON back to the file
+            with open(file_path, "w") as file:
+                json.dump(data, file, indent=4)
+
+
+def move_validation_images(split, percentage):
+    train_dir = f"./dataset/images/{split}"
+    val_dir = "./dataset/images/val"
+    if not os.path.exists(val_dir):
+        os.makedirs(val_dir)
+
+    images = [f for f in os.listdir(train_dir)]
+    num_val_images = int(len(images) * percentage / 100.0)
+    val_images = random.sample(images, num_val_images)
+
+    for image in val_images:
+        src_path = os.path.join(train_dir, image)
+        dest_path = os.path.join(val_dir, image)
+        shutil.move(src_path, dest_path)
+
+
+if __name__ == "__main__":
+    split = "train"
+    file_path = f"/home/mne/Datasets/DriveU/v2.0/DTLD_{split}.json"
+    make_simlinks(file_path, split)
+
+    split = "test"
+    file_path = f"/home/mne/Datasets/DriveU/v2.0/DTLD_{split}.json"
+    make_simlinks(file_path, split)
+
+    move_validation_images("train", 5.0)
