06f10bfaa7f05b56c2b7c42be97884ce12a9d32e
Yesung Hwang <yesung.hw@gmail.com>
2025-04-25 14:27:41 +0200

    Add DINOv2; WIP

--- engine/backbone/__init__.py
+++ engine/backbone/__init__.py
@@ -4,17 +4,15 @@ Copyright(c) 2023 lyuwenyu. All Rights Reserved.
 """
 
 from .common import (
-    get_activation,
     FrozenBatchNorm2d,
     freeze_batch_norm2d,
+    get_activation,
 )
+from .csp_darknet import CSPPAN, CSPDarkNet
+from .csp_resnet import CSPResNet
+from .dinov2 import DINOv2
+from .hgnetv2 import HGNetv2
 from .presnet import PResNet
 from .test_resnet import MResNet
-
 from .timm_model import TimmModel
 from .torchvision_model import TorchVisionModel
-
-from .csp_resnet import CSPResNet
-from .csp_darknet import CSPDarkNet, CSPPAN
-
-from .hgnetv2 import HGNetv2
--- engine/backbone/dinov2.py
+++ engine/backbone/dinov2.py
@@ -1,4 +1,6 @@
+import torch
 import torch.nn as nn
+
 from ..core import register
 
 
@@ -17,6 +19,70 @@ class DINOv2(nn.Module):
         freeze_at=0,
         freeze_norm=True,
         pretrained=True,
-        local_model_dir="weight/hgnetv2/",
+        local_model_dir="weight/dinov2/",
     ):
-        
+        super().__init__()
+        self.use_lab = use_lab
+        self.return_idx = return_idx
+
+        name = name.lower()
+        name = f"dinov2_vit{name}14_reg"
+
+        if pretrained:
+            if torch.distributed.is_initialized():
+                if torch.distributed.get_rank() == 0:
+                    self.network = torch.hub.load("facebookresearch/dinov2", name).to(
+                        "cpu"
+                    )
+                    torch.distributed.barrier()
+                else:
+                    torch.distributed.barrier()
+                    self.network = torch.hub.load("facebookresearch/dinov2", name).to(
+                        "cpu"
+                    )
+            else:
+                self.network = torch.hub.load("facebookresearch/dinov2", name).to("cpu")
+        else:
+            raise NotImplementedError(
+                "DINOv2 IS a pretrained model, please set pretrained=True"
+            )
+
+        self.return_idx = sorted(
+            [
+                idx if idx >= 0 else len(self.network.blocks) + idx
+                for idx in self.return_idx
+            ]
+        )
+
+        if freeze_at >= 0:
+            self._freeze_parameters(self.network.patch_embed)
+            if not freeze_stem_only:
+                for i in range(min(freeze_at + 1, len(self.blocks))):
+                    self._freeze_parameters(self.blocks[i])
+
+        if freeze_norm:
+            self._freeze_norm(self.network)
+
+    def _freeze_norm(self, module):
+        if isinstance(module, nn.LayerNorm):
+            for param in module.parameters():
+                param.requires_grad = False
+        else:
+            for name, child in module.named_children():
+                self._freeze_norm(child)
+
+    def _freeze_parameters(self, module):
+        for p in module.parameters():
+            p.requires_grad = False
+
+    def forward(self, x):
+        x = self.network.patch_embed(x)
+        outs = []
+        for idx, block in enumerate(self.network.blocks):
+            x = block(x)
+            if idx == len(self.network.blocks) - 1:
+                x = self.network.norm(x)
+                x = self.network.head(x)
+            if idx in self.return_idx:
+                outs.append(x)
+        return outs
